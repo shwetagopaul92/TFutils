bf4f8a368ff93dfe144a350d1c880c0c35a5b4b0
Shweta   Gopaulakrishnan <reshg@ride03.bwh.harvard.edu>
2018-03-16 15:32:36 -0400

    changing path to files, adding documentation for data objects, version bump 0.0.2

--- R/tfutils.R
+++ R/tfutils.R
@@ -1,21 +1,21 @@
 
 #' utility to read FIMO outputs from local resource(cluster)
+#' @importFrom IRanges IRanges
+#' @importFrom GenomicRanges GRanges
 #' @param tf character(1) file id
 #' @param which a GRanges delimiting the extract
 #' @examples
 #' requireNamespace("GenomicRanges")
 #' requireNamespace("IRanges")
-#' requireNamespace("DT")
 #' importFIMO_local("M5946_1", which=GenomicRanges::GRanges("chr1", IRanges::IRanges(1,15000)))
 #' @export
 importFIMO_local = function( tf, which ) {
   stopifnot(length(tf)==1, is(tf, "character"))
   chromosome = which@seqnames@values
-  myfile = paste0("/udd/reshg/chrfiles_tf/",tf,"/",chromosome,".bed")
-  chrbed = fread(myfile)
+  myfile = system.file(paste0(tf,"/",chromosome,".bed"), package="TFutils")
+  chrbed = data.table::fread(myfile)
   chrbed
   #chr = plyr::rename(chrbed, c("V1"= "chr","V2"="start","V3"="end","V4"="interval","V5"="score","V6"="strand","V7"="pvalue"))
-  
 }
 
 
@@ -27,9 +27,7 @@ importFIMO_local = function( tf, which ) {
 #' @param conv_name character(1) conventional name for TF
 #' @param map list with elements consisting of the notation for TFs; names of list elements are the conv_names
 #' @param which a GRanges instance limiting the request
-#' @param meth a function with two parameters, tf (a character(1) Mnnnn
-#' string) and which (a GRanges instance) that retrieves TF 'bed'-like
-#' records from a resource
+#' @param meth a function with two parameters, tf (a character(1) string) and which (a GRanges instance) that retrieves TF 'bed'-like records from a resource
 #' @note We assume that the retrieval yields a data.frame in the format
 #' \code{
 #'     V1    V2    V3               V4        V5 V6       V7 \cr
@@ -54,19 +52,19 @@ getBS = function( conv_name, map, which, meth ) {
   ans
 }
 
-#'utility to allow user to retrieve the map of tf name (symbol or motif_id)
-#'@param tf character string with either the motif_id or the hgnc_symbol
-#'@examples
-#' data(motif_id_map)
-#' data(symbol_map)
-#' mapTF("VDR")
-#'@export
-mapTF <- function(tf){
-  if(length(motif_id[[tf]])==1){
-    return(motif_id[[tf]])
-  }
-  else{
-    return(symbol[[tf]])
-  }
-}
+##utility to allow user to retrieve the map of tf name (symbol or motif_id)
+##@param tf character string with either the motif_id or the hgnc_symbol
+##@examples
+## data(motif_id_map)
+## data(symbol_map)
+## mapTF("VDR")
+##@export
+#mapTF <- function(tf){
+ # if(length(motif_id[[tf]])==1){
+  #  return(motif_id[[tf]])
+  #}
+  #else{
+   # return(symbol[[tf]])
+  #}
+#}
 
